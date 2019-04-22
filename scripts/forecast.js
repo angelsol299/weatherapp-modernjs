a55994ec4172eb1fd129de3ad6fd2a415a34344e
angel <angelsol299@hotmail.com>
2019-04-22 22:27:32 +0200

    api weather

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,13 @@
 const key = "0j0Gw2xhvgJjzDDJoTbQ3zFAVAoAklSG";
 
+//geting the weather
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikye=${key}`;
+  const response = await fetch(base + query);
+};
+
+//getting the city
 const getCity = async city => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&&q=${city}`;
