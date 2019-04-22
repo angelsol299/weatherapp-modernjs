79f5378177ac40ef224df342f9629f77bc029615
angel <angelsol299@hotmail.com>
2019-04-22 22:12:50 +0200

    api

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,7 @@
 const key = "0j0Gw2xhvgJjzDDJoTbQ3zFAVAoAklSG";
+
+const getCity = async () => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apiKey=${key}&&q=${city}`;
+  const response = await fetch(base + query);
+};
